96beb484ec9a2f5f023e4444f6296d70de3b5fac
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-03-23 08:15:00 +0100

    Force versions for Puppet modules

--- install-puppet-modules.sh
+++ install-puppet-modules.sh
@@ -2,8 +2,12 @@
 
 echo "Install Puppet modules..."
 
-sudo puppet module install puppetlabs-apache
-sudo puppet module install gajdaw-ubuntu
-sudo puppet module install gajdaw-environment
+sudo puppet module install puppetlabs-apache --version 1.4.0 --force
+sudo puppet module install puppetlabs-concat --version 1.2.0 --force
+sudo puppet module install puppetlabs-stdlib --version 4.5.1 --force
 
+sudo puppet module install gajdaw-ubuntu --version 0.1.13 --force
+sudo puppet module install gajdaw-diverse_functions --version 0.1.1 --force
+
+sudo puppet module install gajdaw-environment --version 0.1.4 --force
 
